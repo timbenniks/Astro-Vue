144aff084972f6c3323bf46eaa55a3e2a52fda5a
Tim Benniks <tbenniks@gmail.com>
2021-08-12 16:45:37 +0200

    initial commit

---
+++ src/components/VideoItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <li class="video-item" :id="video.uid">
+    <a :href="`/videos/${video.uid}`">
+      <img
+        :alt="video.title"
+        :src="video.image"
+        width="16"
+        height="9"
+        loading="lazy"
+        class="w-full object-contain"
+      />
+
+      <article class="my-4">
+        <p class="post-date text-gray-400 text-sm uppercase">
+          {{ video.date }}
+        </p>
+        <h4 class="font-bold text-xl leading-6 mb-2">
+          {{ video.title }}
+        </h4>
+        <p>
+          <span
+            v-for="tag in video.tags"
+            :key="tag"
+            class="
+              inline-block
+              uppercase
+              text-xs
+              bg-blue-100
+              py-1
+              px-2
+              mr-1
+              mb-1
+            "
+          >
+            {{ tag }}
+          </span>
+        </p>
+      </article>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    video: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
